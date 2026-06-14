daf76511d79d435e444497a4b112d67b0e576903
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-14 18:58:45 +0800

    add logic for calculator

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const firstNumber = Number(prompt("Your First Number: "));
 const operation = prompt("Operation: ");
 const secondNumber = Number(prompt("Your Second Number: "));
 
-console.log(typeof firstNumber + typeof operation + typeof secondNumber)
+
 const solve = function(firstNum, opt, secondNum){
     switch (opt){
         case "/":
@@ -13,9 +13,10 @@ const solve = function(firstNum, opt, secondNum){
             return firstNum - secondNum;
         case "+":
             return firstNum + secondNum;
+        case "%":
+            return (firstNum / secondNum) * 100;
         default:
+            console.log("ERROR with solve()")
             return "ERROR";
     }
 }
-
-console.log(solve(firstNumber, operation, secondNumber))
