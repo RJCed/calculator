c5b01c14f2eb2ca62d25c75808c4449ac7a84210
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-15 13:11:32 +0800

    get it working for first iteration

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-let firstNumber = "";
-let operator = "";
-let secondNumber = "";
+let firstNumber = '';
+let operator = '';
+let usedOperator = '';
+let secondNumber = '';
 
 // If finish inputing first number
 let doneFirstInput = false;
 
 //Collection of numbers inputted 
 let firstNumArray = [];
+let secondNumArray = [];
+
+let finalAnswer = '';
+let doneAnswer = false; //set to true after click equals
 
 
+// Get Display output
+const display = document.getElementById("display");
+display.textContent = "|";
+
+//Change display function
+const changeDisplay = function(input){
+
+    if (doneAnswer){
+        display.textContent = finalAnswer;
+    } else if (doneFirstInput === false){
+        display.textContent = `${firstNumber}`;
+        console.log("First Number Display Change")
+    } else if (doneAnswer === false && secondNumber === ''){
+        display.textContent = `${firstNumber} ${input}`;
+        console.log(`Operator Display Change ${input}`)
+    } else if (doneAnswer === false && secondNumber !== ''){
+        display.textContent = `${firstNumber} ${input} ${secondNumber}`;
+        console.log(`Second Number Display Change ${input}`)
+    }
+}
+
+// reset everything (except final answer)
+const reset = function(){
+    firstNumber = '';
+    operator = '';
+    usedOperator = '';
+    secondNumber = '';
+
+    doneFirstInput = false;
+
+    firstNumArray = [];
+    secondNumArray = [];
+
+    finalAnswer = '';
+    doneAnswer = false;
+}
 
 // Solve the given input
 const solve = function(firstNum, opt, secondNum){
@@ -31,25 +72,68 @@ const solve = function(firstNum, opt, secondNum){
 
 //Get the number inputed
 const numberInput = function(num){
-    firstNumArray.push(num);
-    firstNumber = firstNumArray.join('')
-    console.log(firstNumber)
+    if (doneFirstInput === false){
+        firstNumArray.push(num);
+        firstNumber = firstNumArray.join('')
+        changeDisplay();
+        console.log(firstNumber)
+    } else if (doneFirstInput){
+        secondNumArray.push(num);
+        secondNumber = secondNumArray.join('')
+        changeDisplay(usedOperator);
+        console.log(secondNumber);
+    }
+    
 }
 
 
-// Gets the input (from buttons)
+// Converts word into symbols for operation ex. "add" -> "+"  (this is also needed for changing this display)
+const operatorInput = function(operator){
+    switch (operator){
+        case "divide":
+            usedOperator = "÷";
+            changeDisplay("÷");
+            return "/";
+        case "multiply":
+            usedOperator = "×";
+            changeDisplay("×");
+            return "*";
+        case "subtract":
+            usedOperator = "-";
+            changeDisplay("-");
+            return "-";
+        case "add":
+            usedOperator = "+";
+            changeDisplay("+");
+            return "+";
+        case "percent":
+            usedOperator = "%";
+            changeDisplay("%");
+            return "%";
+        default:
+            console.log("ERROR with operatorInput()")
+            return "ERROR";
+    }
+}
+
+// Gets the input (from buttons) ... also changes doneFirstInput if operator button is click
 const buttonContainer = document.getElementById("button-container");
 buttonContainer.addEventListener('mousedown', (event) => {
     const numberButton = event.target.closest('.number');
     const operatorButton = event.target.closest('.operator');
-    
+    const equal = event.target.closest('#equal');
 
     if (numberButton) {
         const numberVal = numberButton.dataset.val;
         numberInput(numberVal);
-    } else if (operatorButton && firstNumber !== '') {
+    } else if (operatorButton && firstNumber !== '' && operator === '') {
         doneFirstInput = true;
         const operatorVal = operatorButton.dataset.action;
-        operator = operatorVal;
+        operator = operatorInput(operatorVal);
+    } else if (equal){
+        finalAnswer = solve(Number(firstNumber), operator, Number(secondNumber));
+        doneAnswer = true;
+        changeDisplay();
+        console.log(finalAnswer);
     }
 })
