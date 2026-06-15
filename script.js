63c95db7cdfab6006213a69c21d41dfa4d4fc4a5
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-15 14:17:07 +0800

    fix bug where operator can change even if already picked

--- script.js
+++ script.js
@@ -3,37 +3,18 @@ let operator = '';
 let usedOperator = '';
 let secondNumber = '';
 
-// If finish inputing first number
+// If finish inputing values
 let doneFirstInput = false;
+let doneOperation = false;
+let doneAnswer = false; //set to true after click equals
 
 //Collection of numbers inputted 
 let firstNumArray = [];
 let secondNumArray = [];
 
 let finalAnswer = '';
-let doneAnswer = false; //set to true after click equals
 
 
-// Get Display output
-const display = document.getElementById("display");
-display.textContent = "|";
-
-//Change display function
-const changeDisplay = function(input){
-
-    if (doneAnswer){
-        display.textContent = finalAnswer;
-    } else if (doneFirstInput === false){
-        display.textContent = `${firstNumber}`;
-        console.log("First Number Display Change")
-    } else if (doneAnswer === false && secondNumber === ''){
-        display.textContent = `${firstNumber} ${input}`;
-        console.log(`Operator Display Change ${input}`)
-    } else if (doneAnswer === false && secondNumber !== ''){
-        display.textContent = `${firstNumber} ${input} ${secondNumber}`;
-        console.log(`Second Number Display Change ${input}`)
-    }
-}
 
 // reset everything (except final answer)
 const reset = function(){
@@ -43,15 +24,40 @@ const reset = function(){
     secondNumber = '';
 
     doneFirstInput = false;
+    doneOperation = false;
+    doneAnswer = false;
 
     firstNumArray = [];
     secondNumArray = [];
 
     finalAnswer = '';
-    doneAnswer = false;
 
     display.textContent = "|";
 }
+
+
+// Get Display output
+const display = document.getElementById("display");
+display.textContent = "|";
+
+//Change display function
+const changeDisplay = function(input){
+
+    if (doneFirstInput === false){
+        display.textContent = `${firstNumber}`;
+        console.log("First Number Display Change")
+    } else if (secondNumber === ''){
+        display.textContent = `${firstNumber} ${input}`;
+        console.log(`Operator Display Change ${input}`)
+    } else if (doneAnswer === false && secondNumber !== ''){
+        display.textContent = `${firstNumber} ${input} ${secondNumber}`;
+        console.log(`Second Number Display Change ${input}`)
+    } else if (doneAnswer){
+        display.textContent = finalAnswer;
+        console.log("Final Answer Display Change")
+    }  
+}
+
 
 // Solve the given input
 const solve = function(firstNum, opt, secondNum){
@@ -138,15 +144,28 @@ buttonContainer.addEventListener('mousedown', (event) => {
     if (numberButton) {
         const numberVal = numberButton.dataset.val;
         numberInput(numberVal);
-    } else if (operatorButton && firstNumber !== '' && operator === '') {
+    } else if (operatorButton && !doneFirstInput && !doneOperation) {
+        doneOperation = true;
         doneFirstInput = true;
         const operatorVal = operatorButton.dataset.action;
         operator = operatorInput(operatorVal);
-    } else if (equal){
+        console.log(operator)
+    } else if (operatorButton && !doneOperation){
+        doneOperation = true;
+        firstNumber = finalAnswer;
+        const operatorVal = operatorButton.dataset.action;
+        operator = operatorInput(operatorVal);
+    }else if (equal && doneFirstInput && secondNumber !== '' && doneOperation){
         finalAnswer = solve(Number(firstNumber), operator, Number(secondNumber));
         doneAnswer = true;
         changeDisplay();
         console.log(finalAnswer);
+        
+        //This is for the next interation
+        secondNumber = '';
+        secondNumArray = [];
+        doneOperation = false;
+        doneAnswer = false;
     } else if (special){
         const specialVal = special.dataset.action;
         specialInput(specialVal);
