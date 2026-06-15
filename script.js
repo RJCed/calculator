b250f8329b2cac36069396814eb754b7839415e7
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-15 11:13:53 +0800

    add event listener to get number input

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const numbers = document.getElementsByClassName("number");
-console.log(numbers)
+const buttonContainer = document.getElementById("button-container");
+
+// Gets the number input (from buttons)
+buttonContainer.addEventListener('mousedown', (event) => {
+    const numberButton = event.target.closest('.number');
+
+    if (!numberButton) return;
+
+    const numberVal = numberButton.dataset.val;
+
+    numberInput(numberVal);
+})
+
+function numberInput(num){
+    console.log(typeof num);
+}
 
 const solve = function(firstNum, opt, secondNum){
     switch (opt){
