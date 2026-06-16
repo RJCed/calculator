ab1398fb05f7cde7025d9e9be31ab87ab51427ba
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-16 11:06:41 +0800

    change length limit of answer from 11 to 10

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const changeDisplay = function(input){
     } else if (doneAnswer){
 
         // Checks greater then 11 (screen size) and if it has a decimal, if so it will round it
-        if (String(finalAnswer).split('').includes('.') && String(finalAnswer).length > 11){
+        if (String(finalAnswer).split('').includes('.') && String(finalAnswer).length > 10){
             let rounded = Math.round(finalAnswer * 100) / 100
             finalAnswer = rounded;
 
@@ -65,7 +65,7 @@ const changeDisplay = function(input){
         display.textContent = finalAnswer;
         
         // Checks if finalAnswer is still greater than 11 digits -> Console.log("Too Big!")
-        if (String(finalAnswer).length > 11){
+        if (String(finalAnswer).length > 10){
             display.textContent = "Too Big!"
         }
 
